179926df5c102d1d202e7872c2e44b7f3a5e5950
ilyosbekolimjonov <codeandcoffeeandme@gmail.com>
2025-12-10 11:49:00 +0500

    feat(db): add modules table and initialize Branch & Course

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { CourseModule } from './modules/course/course.module';
+import { BranchModule } from './modules/branch/branch.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, BranchModule, CourseModule],
   controllers: [AppController],
   providers: [AppService],
 })
