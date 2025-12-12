e74cb2b3c6d11ff53aca5ac60345767e9c6eab25
ilyosbekolimjonov <codeandcoffeeandme@gmail.com>
2025-12-12 11:59:57 +0500

    feat(branch): add basic CRUD operations

--- src/modules/branch/branch.service.ts
+++ src/modules/branch/branch.service.ts
@@ -1,26 +1,73 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateBranchDto } from './dto/create-branch.dto';
 import { UpdateBranchDto } from './dto/update-branch.dto';
 
 @Injectable()
 export class BranchService {
-  create(createBranchDto: CreateBranchDto) {
-    return 'This action adds a new branch';
+  constructor(private prisma: PrismaService) { }
+
+  async create(createBranchDto: CreateBranchDto) {
+    return this.prisma.branch.create({
+      data: createBranchDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all branch`;
+  async findAll() {
+    return this.prisma.branch.findMany({
+      include: {
+        rooms: true,
+        courses: true,
+        _count: {
+          select: {
+            teachers: true,
+            students: true,
+            groups: true,
+          },
+        },
+      },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} branch`;
+  async findOne(id: number) {
+    const branch = await this.prisma.branch.findUnique({
+      where: { id },
+      include: {
+        rooms: true,
+        courses: true,
+        groups: true,
+        teachers: true,
+        students: true,
+        staffs: true,
+      },
+    });
+
+    if (!branch) {
+      throw new NotFoundException(`Branch with ID ${id} not found`);
+    }
+
+    return branch;
   }
 
-  update(id: number, updateBranchDto: UpdateBranchDto) {
-    return `This action updates a #${id} branch`;
+  async update(id: number, updateBranchDto: UpdateBranchDto) {
+    await this.findOne(id); // Check if exists
+
+    return this.prisma.branch.update({
+      where: { id },
+      data: updateBranchDto,
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} branch`;
+  async remove(id: number) {
+    await this.findOne(id);
+
+    await this.prisma.branch.delete({
+      where: { id },
+    });
+
+    return {
+      success: true,
+      message: `Branch with id <${id}> deleted successfully`,
+    };
   }
 }
--- src/modules/branch/dto/create-branch.dto.ts
+++ src/modules/branch/dto/create-branch.dto.ts
@@ -1,1 +1,20 @@
-export class CreateBranchDto {}
+import { IsString, IsOptional, IsEnum, IsNotEmpty } from "class-validator";
+import { Status } from '@prisma/client';
+
+export class CreateBranchDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsOptional()
+    logoUrl?: string;
+
+    @IsString()
+    @IsNotEmpty()
+    address: string;
+
+    @IsEnum(Status)
+    @IsOptional()
+    status?: Status;
+}
